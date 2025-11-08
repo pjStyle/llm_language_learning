b2cf243ba6d58c86d57484a128eb2c077d1282e8
pjStyle <devted@gmail.com>
2025-11-08 13:03:14 +0900

    Changes to protocol
    talk_to_llm changed to GET with url parameter
    Simple error handling

--- app.py
+++ app.py
@@ -10,17 +10,22 @@ CORS(app)
 def health_check():
     return jsonify({"status": "ok", "message": "Server is running"})
 
-@app.route('/api/talk_to_llm', methods=['POST'])
+@app.route('/api/talk_to_llm', methods=['GET'])
 def talk_to_llm():
-    data = request.get_json()
-    print(data)
-    response = ollama.chat(
-        model = 'llm_language', 
-        messages=
-        [
-            {'role': 'user', 'content': data},
-        ],)
-    return response['message']['content']
+    json_request = request.args.get('json', '')
+    print("talk_to_llm")
+    if (json_request != ''):
+        response = ollama.chat(
+            model = 'llm_language', 
+            messages=
+            [
+                {'role': 'user', 'content': json_request},
+            ],)
+        response_to_send = response['message']['content']
+        print(response_to_send)
+        return response_to_send
+    
+    return jsonify({"error": "Invalid request"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
